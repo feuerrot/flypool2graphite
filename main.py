e42802a45e4b42892f2974c4478d1c65a72ab685
Markus Witt <feuerrot@feuerrot.org>
2017-09-15 02:22:52 +0200

    main: add more than one pool

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ config = {
 	'graphite_prefix': 'mining.stats.{}.',
 	'miner': [
 		'',
-	],
-	'stats_url': 'https://api-zcash.flypool.org/miner/{}/currentStats'
+	]
 }
 
 def get_stats(worker):
-	r = requests.get(config['stats_url'].format(worker))
+	r = requests.get(worker)
 	j = r.json()
 	return j
 
@@ -20,13 +19,16 @@ def add_worker_data(worker, graphite):
 	data = get_stats(worker)
 	if data['status'] != 'OK':
 		print('[!] Error: status for worker {} not OK:\n    {}'.format(worker, data))
+	print(data)
 	graphite.send_dict(data['data'])
 
 if __name__ == '__main__':
 	for worker in config['miner']:
 		graphite = graphitesend.init(
 			graphite_server = config['graphite_server'],
-			prefix = config['graphite_prefix'].format(worker),
+			prefix = config['graphite_prefix'].format(
+				worker.split('/')[4]
+			),
 			system_name = ''
 		)
 		add_worker_data(worker, graphite)
